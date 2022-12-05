3ba07d8f2563880802b6e6989b9f67045de7235c
sonnguyenhong <sonnguyenhong291@gmail.com>
2022-12-05 17:54:16 +0700

    back-001: add login api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -21,7 +21,24 @@ const registerUser = async (req, res) => {
     }
 };
 
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+    try {
+        const params = {
+            ...req.body,
+        };
+        const response = await authService.loginUser(params);
+        return res.status(httpStatus.OK).json({
+            status: apiStatus.SUCCESS,
+            message: 'Login successfully!',
+            data: response,
+        });
+    } catch (err) {
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+            status: apiStatus.OTHER_ERROR,
+            message: err.message,
+        });
+    }
+};
 
 module.exports = {
     registerUser,
--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,4 +1,6 @@
 const User = require('../models').User;
+const { compare } = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const registerUser = async (params) => {
     const username = params.username;
@@ -24,7 +26,40 @@ const registerUser = async (params) => {
     };
 };
 
-const loginUser = async (params) => {};
+const loginUser = async (params) => {
+    const username = params.username;
+    const password = params.password;
+
+    const user = await User.findOne({
+        where: {
+            username: username,
+        },
+    });
+
+    if (!user) {
+        throw new Error('User does not exist');
+    }
+
+    const isCorrectPassword = await compare(password, user.password);
+    if (!isCorrectPassword) {
+        throw new Error('Password is incorrect');
+    }
+
+    const userInfo = {
+        id: user.id,
+        username: user.username,
+    };
+
+    const token = jwt.sign(userInfo, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRATION,
+    });
+
+    return {
+        ...user.dataValues,
+        password: null,
+        token,
+    };
+};
 
 module.exports = {
     registerUser,
